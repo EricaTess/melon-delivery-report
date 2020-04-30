6167e792c72432be159edcd3bd2379a88e11db85
Erica Sanchez <sanchez.ericat@gmail.com>
2020-04-30 03:33:13 +0000

    made delivery function

--- produce_summary.py
+++ produce_summary.py
@@ -1,43 +1,17 @@
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+def delivery_list(day, file):
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
+    print(f"Day {day}")
+    the_file = open(f"{file}")
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
 
+        print("Delivered {} {}s for total of ${}".format(
+            count, melon, amount))
+    the_file.close()
 
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
-
-
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+delivery_list(1, "um-deliveries-20140519.txt")
